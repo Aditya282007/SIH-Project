2767a9b2f498f966def677bccac14ff02b53e03e
Aditya282007 <unstoppableadi28@gmail.com>
2024-09-20 22:52:14 +0530

    Home page

--- Home Page/app.js
+++ Home Page/app.js
@@ -1,26 +1,13 @@
-// Slider
-const slides = document.querySelectorAll('.slide');
+//Fade btn
+const arrow =document.getElementById("arrow");
 
-let currentSlide = 0;
-
-slides.forEach((slide, index) => {
-  slide.style.transform = `translateX(${index * 100}%)`;
-  if (index === 0) {
-    slide.classList.add('active');
-  }
-});
-
-function nextSlide() {
-  slides[currentSlide].classList.remove('active');
-  currentSlide = (currentSlide + 1) % slides.length;
-  slides[currentSlide].classList.add('active');
-  slides.forEach((slide, index) => {
-    slide.style.transform = `translateX(${(index - currentSlide) * 100}%)`;
-  });
-}
-
-// Call nextSlide every 5 seconds
-setInterval(nextSlide, 5000);
+window.addEventListener("scroll",() => {
+    let value = window.scrollY;
+    if (value <= 5) {
+        arrow.classList.toggle('fade-in');
+        arrow.classList.toggle('fade-out');
+    }
+})
 
 // Scroll reveal
 const revealElements = document.querySelectorAll("[data-reveal]");
